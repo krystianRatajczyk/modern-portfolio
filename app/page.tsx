d5900283b46d6bcac2d806926298cbd30059b242
krystianRatajczyk <ratajczyk.krystian7@gmail.com>
2024-08-01 18:56:36 +0200

    approach added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
@@ -10,14 +11,13 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
